9c2eab5353526f7be3cf576c3645b025c695a6a5
Joe Brew <joebrew@gmail.com>
2014-09-29 13:53:44 -0400

    playing with census stuff

---
+++ census_acs_choroplethr.R
@@ -0,0 +1,12 @@
+library(choroplethr)
+library(acs)
+
+my_key <- "e6dbe1347a39270d90d300f78179adcb87a596d2"
+api.key.install(my_key)
+
+choroplethr_acs("B19001", "county", endyear=2012, span=5,
+                showLabels = FALSE)
+
+# B01003: total population
+# B01002: median age by sex
+# B19001: household income in the last 12 months
