def63756e5b58705c08231c7acc89d480b886826
Joe Brew <joebrew@gmail.com>
2014-09-30 17:49:14 -0400

    stuff for grooveshark

--- gnv_crime/gainesville_crime.R
+++ gnv_crime/gainesville_crime.R
@@ -1,22 +1,32 @@
+#########
+# SET LOCAL WORKING DIRECTORY
+#########
 setwd("C:/Users/BrewJR/Documents/misc") # change this line to whereever you cloned misc
 setwd("gnv_crime")
 
+
+#########
+# READ IN GAINESVILLE CRIME DATA
+#########
 # define the link for gainesville crime
 my_link1 <- "https://data.cityofgainesville.org/api/views/9ccb-cyth/rows.csv"
 
 # read in the data
 gnv <- read.csv(my_link1)
 
+#########
+# CLEAN IT UP A BIT
+#########
 # write a function to clean lat lon points
-DegreesToDecimals <- function(x){
+JoeFun <- function(x){
   
   # split the string to keep only the lat, lon, part
   a <- do.call(rbind, strsplit(as.character(x), "\n"))
   aa <- a[,3]
   
-    # now split at the comma
-    b <- do.call(rbind, strsplit(as.character(aa), ","))
-    bb <- b#[,2]
+  # now split at the comma
+  b <- do.call(rbind, strsplit(as.character(aa), ","))
+  bb <- b#[,2]
   
   # make df
   bb <- data.frame(bb)
@@ -30,11 +40,26 @@ DegreesToDecimals <- function(x){
   
   return(bb)
 }
-x <- DegreesToDecimals(gnv$Location.1)
+x <- JoeFun(gnv$Location.1)
 # now join x to gnv
 gnv <- cbind(gnv, x)
 rm(x)
 
+# Make a date column
+gnv$date <- as.Date(substr(gnv$Offense.Date,1,10), format = "%m/%d/%Y")
+
+# view it
+hist(gnv$date, breaks = 100)
+
+# Remove dates prior to 2011
+gnv <- gnv[which(gnv$date > "2011-01-01"),]
+hist(gnv$date, breaks = 100)
+
+
+
+#########
+# MAKE SOME MAPS
+#########
 # plot the points on a florida map
 library(maps)
 map("county", "florida")
@@ -45,6 +70,7 @@ library(rgdal)
 library(sp)
 zip <- readOGR("zips_alachua", "ACDPS_zipcode")
 zip <- spTransform(zip, CRS("+init=epsg:4326"))
+zip$zip <- as.numeric(as.character(zip$ZIP))
 plot(zip, col = "grey", border = "white")
 
 points(gnv$lon, gnv$lat,
@@ -54,8 +80,7 @@ points(gnv$lon, gnv$lat,
 
 # Color by crime type ("Narrative")
 library(RColorBrewer)
-my_colors <- colorRampPalette(brewer.pal(3, "Spectral"))(length(levels(gnv$Narrative)))
-barplot(summary(gnv$Narrative), col = my_colors, border = FALSE)
+my_colors <- colorRampPalette(rainbow(10))(length(levels(gnv$Narrative)))
 
 gnv$col <- my_colors[as.numeric(gnv$Narrative)]
 plot(zip, col = "grey", border = "white")
@@ -64,6 +89,78 @@ points(gnv$lon, gnv$lat,
        col = adjustcolor(gnv$col, alpha.f = 0.5),
        pch = 16,
        cex = 0.5)
+
+legend(x = "bottomleft",
+       pch = 16,
+       col = adjustcolor(my_colors, alpha.f = 0.8),
+       pt.cex = 0.1,
+       cex = 0.1,
+       ncol = 6, 
+       bty = "n",
+       legend = substr(levels(gnv$Narrative), 1, 5))
+
+########
+# GET ZIP CODE FOR EACH CRIME
+########
+# make a spatial version of gnv
+gnv <- gnv[which(!is.na(gnv$lat) & !is.na(gnv$lon)),]
+gnv_sp <- SpatialPointsDataFrame(gnv[,c("lon", "lat")], gnv,
+                                 proj4string = CRS("+init=epsg:4326"))
+x <- over(gnv_sp, polygons(zip))
+
+# Return to dataframe
+gnv$zip <- zip$zip[x]
+
+#########
+# GET NUMBER OF CRIMES BY ZIP CODE
+#########
+library(dplyr)
+crimes_zip <- 
+  gnv %>%
+  group_by(zip) %>%
+  summarise(n_crimes = n()) %>%
+  arrange(desc(n_crimes))
+
+# bring those numbers into the spatial polygons data frame
+zip_df <- data.frame(zip)
+
+zip_df <- merge(x = zip_df,
+                y = crimes_zip,
+                by = "zip",
+                all.x = TRUE,
+                all.y = FALSE)
+zip$n_crimes <- zip_df$n_crimes
+
+##########
+# WRITE A FUNCTION FOR MAKING A CHOROPLETH MAP OF THESE ZIP CODES
+##########
+library(classInt)
+MapFun <- function(var, color){
+  plotvar <- var
+  nclr <- 8
+  plotclr <- brewer.pal(nclr, color)
+  class <- classIntervals(plotvar, nclr, style = "quantile", dataPrecision=0) #use "quantile" instead
+  #class <- classIntervals(0:100, nclr, style="equal")
+  colcode <- findColours(class, plotclr)
+  legcode <- paste0(gsub(",", " - ", gsub("[[]|[]]|[)]", "", names(attr(colcode, "table")))))
+  plot(zip, border="darkgrey", col=colcode)
+  legend("bottomleft", # position
+         legend = legcode, #names(attr(colcode, "table")), 
+         fill = attr(colcode, "palette"), 
+         cex = 0.8, 
+         border=NA,
+         bty = "n",
+         y.intersp = 0.6)
+}
+
+# Now plot
+MapFun(zip$n_crimes, "Blues")
+title(main = "Number of crimes by zip code")
+
+# Plot rate (adj. for pop)
+MapFun(zip$n_crimes / zip$POP1996 * 100000, "Blues")
+title(main = "Crime rate by zip code")
+
 
 # PLOT WITH GOOGLE
 library(googleVis)
@@ -89,13 +186,14 @@ plot(g.inter.map)
 library(rCharts)
 
 # read in geojson version of zip
-zip_geoj <- readOGR("zips_alachua", "ACDPS_zipcode")
+#zip_geoj <- readOGR("zips_alachua", "ACDPS_zipcode")
 mymap <- Leaflet$new()
 mymap$tileLayer(provider = "Stamen.TonerLite")
 mymap$setView(c(29.55, -82.12), zoom = 10)
 mymap$enablePopover(TRUE)
-mymap$geoJson(zip_geoj)
-#mymap$fullScreen(TRUE)
+mymap$geoJson(zipgj)
+
+mymap$fullScreen(TRUE)
 
 for (i in 1:100){
   mymap$marker(c(gnv$lat[i], gnv$lon[i]),
@@ -106,6 +204,7 @@ for (i in 1:100){
 
 mymap
 
+# http://zevross.com/blog/2014/04/11/using-r-to-quickly-create-an-interactive-online-map-using-the-leafletr-package/
 # Clean up zip code stuff to add to it
 
 # give better variable name
@@ -120,9 +219,7 @@ library(rgdal) #for reading/writing geo files
 library(rgeos) #for simplification
 library(sp)
 
-# note that this file is somewhat big so it might take a couple
-# of minutes to download
-#url<-"http://www2.census.gov/geo/tiger/TIGER2010DP1/County_2010Census_DP1.zip"
+
 downloaddir<-paste0(getwd(), "/zips_alachua" )
 
 filename<-list.files(downloaddir, pattern=".shp", full.names=FALSE)
@@ -130,8 +227,6 @@ filename<-gsub(".shp", "", filename)
 
 # ----- Change zip to dat
 dat <- zip
-
-# ----- Create a subset of New York counties
 subdat<- dat
 
 # ----- Transform to EPSG 4326 - WGS84 (required)
@@ -144,7 +239,7 @@ names(subdat)[names(subdat) == "POP1996"]<-"Population"
 subdat_data<- subdat@data
 
 # ----- simplification yields a SpatialPolygons class
-subdat<-gSimplify(subdat,tol=0.01, topologyPreserve=TRUE)
+# subdat<-gSimplify(subdat,tol=0.01, topologyPreserve=TRUE)
 
 # ----- to write to geojson we need a SpatialPolygonsDataFrame
 subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
@@ -154,19 +249,22 @@ subdat<-SpatialPolygonsDataFrame(subdat, data=subdat_data)
 # ----- Write data to GeoJSON
 leafdat<-paste(downloaddir, "/", filename, ".geojson", sep="") 
 
-# ------ Function to write to GeoJSON
-togeojson <- function(file, writepath = "~") {
-  url <- "http://ogre.adc4gis.com/convert"
-  tt <- POST(url, body = list(upload = upload_file(file)))
-  out <- content(tt, as = "text")
-  fileConn <- file(writepath)
-  writeLines(out, fileConn)
-  close(fileConn)
-}
+#zipgj <- toGeoJSON(data = zip, dest = paste0(getwd(),"/zips_alachua"))
+zipgj <- toGeoJSON(data = subdat, dest = paste0(getwd(),"/zips_alachua"))
 
-library(httr)
-file <- paste0(getwd(), "/zips_alachua/", "ACDPS_zipcode.zip")
-togeojson(file, leafdat)
+# # ------ Function to write to GeoJSON
+# togeojson <- function(file, writepath = "~") {
+#   url <- "http://ogre.adc4gis.com/convert"
+#   tt <- POST(url, body = list(upload = upload_file(file)))
+#   out <- content(tt, as = "text")
+#   fileConn <- file(writepath)
+#   writeLines(out, fileConn)
+#   close(fileConn)
+# }
+# 
+# library(httr)
+# file <- paste0(getwd(), "/zips_alachua/", "ACDPS_zipcode.zip")
+# togeojson(file, leafdat)
 
 
 # ----- Create the cuts
@@ -183,10 +281,83 @@ sty<-styleGrad(prop="Population", breaks=cuts, right=FALSE, style.par="col",
                style.val=rev(heat.colors(6)), leg="Population", lwd=1)
 
 
+
+
 # ----- Create the map and load into browser
-map<-leaflet(data=leafdat, dest=downloaddir, style=sty,
+map<-leaflet(data=zipgj, dest=downloaddir, style=sty,
              title="index", base.map="osm",
              incl.data=TRUE,  popup=popup)
 
 # ----- to look at the map you can use this code
 browseURL(map)
+
+
+####################
+install.packages("rgbif")
+library(rgbif)
+file2 <-  "/zips_alachua/ACDPS_zipcode.shp"
+## Success! File is at /Users/scottmac2/abiesmagmap.geojson
+gist(paste0(getwd(),"/zips_alachua/zip.geojson"), description = "test")
+
+#################################
+# MODELING
+##################################
+
+# Let's just look at 2013
+gnv <- gnv[which(gnv$date >= "2013-01-01" &
+                   gnv$date <= "2013-12-31"),]
+
+#install.packages("weatherData")
+library(weatherData)
+
+# Get weather for just one day
+getWeatherForDate("GNV", "2014-04-14")
+
+# Get weather for a period of time
+weather <- getSummarizedWeather("GNV", start_date = min(gnv$date), 
+                                end_date = max(gnv$date),
+                                opt_custom_columns = TRUE,
+                                custom_columns = c(2,4,20))
+
+# format
+weather$date <- as.Date(weather$Date, format = "%Y-%m-%d")
+
+# Merge to gnv
+gnv <- merge(x = gnv,
+             y = weather,
+             by = "date",
+             all.x = TRUE,
+             all.y = FALSE)
+
+
+# Glimpse at your data
+head(weather)
+
+# Clean
+weather$rain <- as.numeric(weather$PrecipitationIn)
+
+
+# Plot max temperature
+plot(weather$date, weather$Max_TemperatureF, 
+     col = adjustcolor("darkred", alpha.f = 0.3),
+     pch = 16,
+     ylim = c(20, 100),
+     xlab = "Date",
+     ylab = "Temp")
+
+# Add min temperature
+points(weather$date, weather$Min_TemperatureF, 
+       col = adjustcolor("darkblue", alpha.f = 0.3),
+       pch = 16)
+
+# Add legend
+legend("bottom",
+       pch = 16,
+       col = adjustcolor(c("darkred", "darkblue"), alpha.f = 0.3),
+       legend = c("High", "Low"),
+       bty = "n")
+
+# Where are we?
+abline(v = Sys.Date() - 365,
+       col = adjustcolor("black", alpha.f = 0.4),
+       lwd = 2)
