49594b3eaaee5c83584634b7f678578250d0eafc
Joe Brew <joebrew@gmail.com>
2014-11-25 15:05:08 -0500

    dot map function for density by dots

--- dot_map/dot_map.R
+++ dot_map/dot_map.R
@@ -8,7 +8,6 @@ if ( Sys.info()["sysname"] == "Linux" ){
 }
 setwd(public) 
 
-
 #####
 # ATTACH PACKAGES
 #####
@@ -30,60 +29,27 @@ collapse_map <- function(x){
 
 ct_boundary <- collapse_map(ct)
 
-#######
-# FAST ATTEMPT
-##############
-plot(ct, col = "white", border = "white")
-for (i in 1:nrow(ct)){ 
-  temp <- ct[i,]
-  
-  n_dots <- temp$pop
-  
-  # Generate random points x and y
-  x <- runif(100000, 
-             min= temp@bbox[,"min"]["x"], 
-             max= temp@bbox[,"max"]["x"])
-  y <- runif(100000,
-             min= temp@bbox[,"min"]["y"], 
-             max= temp@bbox[,"max"]["y"])
-  
-  # Merge points into my_points (spatial points)
-  my_points <- data.frame(cbind(x,y))
-  coordinates(my_points) <- ~x+y
-  proj4string(my_points) <- proj4string(temp)
-  
-  # Whic of the polygons does each point fall into
-  in_points <- over(my_points, polygons(temp))
-  
-  # Plot points in each polygon
-  # Get the points for each polygon
-  plot_these <- sample(my_points[which(!is.na(in_points))], n_dots, replace = FALSE)
-  
-  # Jitter the points
-  #plot_these <- jitter(coordinates(plot_these))
-  
-  # Plot the points
-  points(plot_these, pch = 1, cex = 0.1,
-         col = adjustcolor("black", alpha.f = 0.1))
-}
-
 #########
-# FAST ATTEMPT WRAPPED INTO A FUNCTION
+# FUNCTION TO PLOT WITH DOT DENSITY
 #########
 
 DotFun <- function(main_shape,
-                   points_number = 100000,
+                   points_number = 10000,
                    points_var,
                    points_col = "darkred",
                    cex = 0.01,
                    border = "white",
-                   fill = "white"){
+                   fill = "white",
+                   alpha = 0.1){
   
   plot(main_shape, col = fill, border = border)
   for (i in 1:nrow(main_shape)){ 
     temp <- main_shape[i,]
     
-    n_dots <- points_var#temp@data[,points_var]
+    n_dots <- points_var[i] #temp@data[,points_var]
+    
+    # Get bare minimum number of points,
+    # Generate more if necessary
     
     # Generate random points x and y
     x <- runif(points_number, 
@@ -101,20 +67,43 @@ DotFun <- function(main_shape,
     # Whic of the polygons does each point fall into
     in_points <- over(my_points, polygons(temp))
     
+    # While loop to generate more points if needed
+    while(length(in_points[which(!is.na(in_points))]) < n_dots){
+      
+      # Generate more
+      points_number <- points_number*10
+      # Generate random points x and y
+      x <- runif(points_number, 
+                 min= temp@bbox[,"min"]["x"], 
+                 max= temp@bbox[,"max"]["x"])
+      y <- runif(points_number,
+                 min= temp@bbox[,"min"]["y"], 
+                 max= temp@bbox[,"max"]["y"])
+      
+      # Merge points into my_points (spatial points)
+      my_points <- data.frame(cbind(x,y))
+      coordinates(my_points) <- ~x+y
+      proj4string(my_points) <- proj4string(temp)
+      
+      # Whic of the polygons does each point fall into
+      in_points <- over(my_points, polygons(temp))
+      
+    }
+    
     # Get the points for each polygon
     plot_these <- sample(my_points[which(!is.na(in_points))], n_dots, replace = FALSE)
-
+    
     # Plot the points
     points(plot_these, pch = 1, cex = cex,
-           col = adjustcolor(points_col, alpha.f = 0.1))
+           col = adjustcolor(points_col, alpha.f = alpha))
   }
 }
 
 DotFun(main_shape = ct,
-       points_number = 10000,
-       points_var = ct$pop ,
+       points_number = 1000,
+       points_var = ct$pop,
        points_col = "darkred",
-       cex = 0.01,
+       cex = 0.001,
        border = "black",
-       fill = "white")
-
+       fill = "grey",
+       alpha = 0.5)
