f38b749c1defa26963bb59a5049a30b328aac085
joebrew <joebrew@gmail.com>
2014-12-20 13:35:56 -0500

    leaflet stuff!

--- leaflet_learn/leaflet_learn.R
+++ leaflet_learn/leaflet_learn.R
@@ -1,4 +1,9 @@
+# Joe's code, but a lot borrowed from
 # http://zevross.com/blog/2014/04/11/using-r-to-quickly-create-an-interactive-online-map-using-the-leafletr-package/
+
+#####
+# ATTACH LIBRARIES (some of these need to be installed from github, jcheng, ramnathv, etc.)
+#####
 library(leaflet)
 library(RColorBrewer)
 library(maps)
@@ -8,7 +13,6 @@ library(leafletR)
 library(rgeos) #for simplification
 library(sp)
 library(ggmap)
-
 
 #####
 # DIRECTORIES
@@ -35,42 +39,47 @@ fl_df <- fortify(fl, region = "NAME")
 each_county <- split(fl_df, fl_df$group)
 
 #####
-#
+# ASSIGN THE VARIABLE WE WANT TO MAP
 #####
-
-
-download_dir<-paste0(getwd(), "/counties")
-
-file_name <- list.files(download_dir, pattern=".shp", full.names=FALSE)
-file_name <- gsub(".shp", "", filename)
-
-# assign the field we want to map
 fl$var <- fl@data[,"N3"]
 
-# # ----- save the data slot
-fl_data<- fl@data
+#####
+# SETUP SOME PARAMETERS FOR WHERE TO READ AND WRITE GEOJSON FILES
+# AND DO
+#####
 
-# ----- Write data to GeoJSON
+download_dir<-paste0(getwd(), "/counties")
+file_name <- list.files(download_dir, pattern=".shp", full.names=FALSE)
+file_name <- gsub(".shp", "", file_name)
+
+#  Write data to GeoJSON
 leafdat <- paste(download_dir, "/", file_name, ".geojson", sep="") 
-
 zipgj <- toGeoJSON(data = fl, dest = paste0(getwd(),"/output"))
 
-
-# ----- Create the cuts
+#####
+# ESTABLISH CHOROPLETH PARAMETERS
+#####
+#  Create the cuts
 cuts<-round(quantile(fl$var, probs = seq(0, 1, 0.20), na.rm = FALSE), 0)
-cuts[1] <- 0 # ----- for this example make first cut zero
+cuts[1] <- 0 #  for this example make first cut zero
 
-# ----- Fields to include in the popup
+#  Fields to include in the popup
 popup <- c("NAME", "var")
 
-# ----- Gradulated style based on an attribute
+#  Gradulated style based on an attribute
 sty <- styleGrad(prop="var", breaks=cuts, right=FALSE, style.par="col",
                style.val=brewer.pal(5, "Reds"), leg="var", lwd=1)
 
-# ----- Create the map and load into browser
+#####
+# CREATE LEAFLET CLASS MAP OBJECT
+#####
+
+#  Create the map and load into browser
 map <- leaflet(data=zipgj, dest=download_dir, style=sty,
-             title="index", base.map="toner",
+             title="index", base.map=c("mqsat", "osm", "tls", "mqosm", "toner", "water"), 
              incl.data=TRUE,  popup=popup)
 
-# ----- to look at the map you can use this code
+#####
+# VIEW MAP IN BROWSER
+#####
 browseURL(map)
