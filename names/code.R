d218a5e161f9f153f388079af09b07a2b2d9985f
joebrew <joebrew@gmail.com>
2014-12-19 19:15:02 -0500

    more names

--- names/code.R
+++ names/code.R
@@ -11,9 +11,34 @@ all <- readLines("/home/joebrew/Documents/misc/names/NamesDatabases/first names/
                 warn = FALSE)
 all <- data.frame(name = all, country = "all")
 
-# Merge
-library(dplyr)
-pick <- inner_join(x = us,
-                y = es,
-                by = "name")
-pick$name
+balears <- read.csv("/home/joebrew/Documents/misc/names/balears.csv", skip = 8,
+                    stringsAsFactors = FALSE)
+names(balears) <- c("name", "illes_balears", "mallorca", "petra", "x")
+
+library(XML)
+x <- xmlToList("catala.xml")
+my_list <- list()
+for (i in 1:length(x)){
+  my_name <- paste0("df", i)
+  
+  assign(my_name, data.frame(matrix(unlist(x[[i]]), nrow = 1, byrow = T)))
+    
+  my_list[[i]] <- get(my_name)
+}
+catala <- do.call(rbind.fill, my_list)
+names(catala)[1:2] <- c("nom", "nombre") 
+catala$nom <- as.character(catala$nom)
+catala$nombre <- as.character(catala$nombre)
+
+
+random_name <- function(df = balears, var_name = "name"){
+  sample(df[,var_name], 1)
+}
+random_name()
+
+# Sample
+x <- sample(balears$name, nrow(balears))
+for (i in 1:length(x)){print(x[i]);Sys.sleep(1)}
+
+# Sample
+sample(all$name, 1)
