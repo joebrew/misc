53a2272ed6b3e7c7a70cbd77119ad6019848d7b8
joebrew <joebrew@gmail.com>
2014-12-20 01:14:02 -0500

    playing around with rmaps

---
+++ rmaps_learn/rmaps_learn.R
@@ -0,0 +1,26 @@
+#####
+# INSTALL AND ATTACH rMaps LIBRARY
+#####
+require(devtools)
+install_github('ramnathv/rCharts@dev')
+install_github('ramnathv/rMaps')
+library(rMaps)
+
+#####
+# CROSSLET EXAMPLE
+#####
+crosslet(
+  x = "country", 
+  y = c("web_index", "universal_access", "impact_empowerment", "freedom_openness"),
+  data = web_index
+)
+
+#####
+# DATA MAPS
+#####
+ichoropleth(Crime ~ State, data = subset(violent_crime, Year == 2010))
+ichoropleth(Crime ~ State, data = violent_crime, animate = "Year")
+ichoropleth(Crime ~ State, data = violent_crime, animate = "Year", play = TRUE)
+
+# explore violent crime data
+violent_crime <- violent_crime
