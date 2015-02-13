c5ac59ea4b65def23fa697d8743f8b3f0d9e6593
joebrew <joebrew@gmail.com>
2015-02-13 16:28:42 -0500

    pretty map

--- python_gis/python_gis.py
+++ python_gis/python_gis.py
@@ -16,3 +16,10 @@ m = Basemap(width=12000000,height=9000000,projection='lcc',
             resolution=None,lat_1=45.,lat_2=55,lat_0=50,lon_0=-107.)
 m.etopo()
 plt.show()
+
+# setup Lambert Conformal basemap.
+# set resolution=None to skip processing of boundary datasets.
+m = Basemap(width=12000000,height=9000000,projection='lcc',
+            resolution=None,lat_1=45.,lat_2=15,lat_0=10,lon_0=20)
+m.bluemarble()
+plt.show()
